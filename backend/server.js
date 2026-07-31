c22710ce7ec47b6f7d7a35a5b50605bb48934aab
Prince Yadav <167596785+warrior-hub@users.noreply.github.com>
2026-08-01 00:39:23 +0530

    Modify CORS settings for specific origin
    Updated CORS configuration to allow specific origin and credentials.

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://mern-ecommerce-1-awd9.onrender.com",
+  credentials: true
+}));
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
